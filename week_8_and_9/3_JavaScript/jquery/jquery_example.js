be9d6882fb0f94b5f23d46bfca31fc732a67f707
lrakhman <lana.rakhman@gmail.com>
2014-05-10 13:48:44 -0500

    jquery challenge

--- week_8_and_9/3_JavaScript/jquery/jquery_example.js
+++ week_8_and_9/3_JavaScript/jquery/jquery_example.js
@@ -7,19 +7,42 @@ $('body').css({'background-color': 'pink'})
  
 //RELEASE 1:
   //Add code here to select elements of the DOM 
+  var bodyElement = $('body');
  
  
 //RELEASE 2: 
   // Add code here to modify the css and html of DOM elements
+
+$('h1').css({'color': 'purple'}) 
+$('h1').css({'border': '5px solid black'})
+$('h1').css({'visibility': 'visible' })
+
+$('.mascot h1').html('FIREFLIES')
  
- 
-//RELEASE 3: Event Listener
+//RELEASE 3: Event Listener 
   // Add the code for the event listener here 
+
+  $('img').on('mouseover', function(e){
+     e.preventDefault()
+    $(this).attr('src', 'http://static.ddmcdn.com/gif/firefly.jpg')
+  });
+
+  $('img').on('mouseleave', function(e){
+  	e.preventDefault()
+  	$(this).attr('src', 'http://cdn1.arkive.org/media/7B/7B2DF3C9-CE99-4F54-9ACC-BFDBED4E8081/Presentation.Large/Ad%C3%A9lie-penguin-walking.jpg')
+  });
  
  
 //RELEASE 4 : Experiment on your own
  
- 
+ $( ".mascot" ).click(function() {
+  $( "img" ).animate({
+    width: "30%",
+    marginLeft: "0.6in",
+    fontSize: "3em",
+    borderWidth: "10px"
+  }, 1500 );
+});
  
  
  
