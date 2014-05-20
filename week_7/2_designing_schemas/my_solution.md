bddb04d4a35452ecb30881d1aa235d7bc1c65246
whoisglover <dannyglover@outlook.com>
2014-05-20 00:54:41 -0700

    challenge 2 and gps 3 content

--- week_7/2_designing_schemas/my_solution.md
+++ week_7/2_designing_schemas/my_solution.md
@@ -1,29 +1,35 @@
 # U3.W7: Designing Schemas
 
 
-#### I worked on this challenge [by myself, with: ]
+#### I worked on this challenge by myself.
 
 
 ## Release 0: Student Roster Schema
 <!-- display your image inline here -->
-
+![image](../imgs/1_student_roster.jpg)
 
 ## Release 1: One to Many Schema
 <!-- display your image inline here -->
-
+![image](../imgs/2_one_to_many.jpg)
 
 ## Release 2: One to One Schema
 <!-- display your image inline here -->
-
+![image](../imgs/3_one_to_one.jpg)
 
 ## Release 3: Many to Many Schema
 <!-- display your image inline here -->
+![image](../imgs/4_many_to_many.jpg)
 
 
 ## Release 4: Design your own Schema
 Description of what you're modeling: 
+I decided to model a school.  For the many to many I represent classes and students.  Students are enrolled in many classes and each class has many students in it.  For the one to one schema I modeled schools and principals.  Each school has a single principal and each principal is responsible for one school. 
 
 <!-- display your one-to-one image inline here -->
+![image](../imgs/my_one_to_one.jpg)
+
 <!-- display your many-to-many image inline here -->
+![image](../imgs/my_many_to_many.jpg)
 
 ## Release 5: Reflection
+	
--- week_7/7_gps3.1/our_solution.rb
+++ week_7/7_gps3.1/our_solution.rb
@@ -1,34 +1,135 @@
 # U3.W7: Build an Electronic Grocery List!
  
 # Your full names:
-# 1.
-# 2.
+# 1. Danny Glover
+# 2. Sherwood Callaway
  
-# User Stories (As many as you want. Delete the statements you don't need)
-# As a user, I want to
-# As a user, I want to
-# As a user, I want to
-# As a user, I want to
-# As a user, I want to
- 
-# Pseudocode
-# 
-# 
-# 
-# 
-# 
-# 
- 
- 
-# Your fabulous code goes here....
+## Release 0: User Stories
+# With your partner, talk about what you would like to do with your grocery list. Start each statement with "As a user, I want to ..." These are what we call "user stories."
+
+# create an empty shopping list
+# add a new item to the shopping list
+# remove an item from the shopping list
+# change item status from "need" to "have"
+# alphabetize the shopping list
+# return all the items in one category
+# return the items in order of price
+# view the entire list 
+
+# Release 1: Pseudocode
+# What objects, classes, and methods will you need to do each of the things you identified in your user stories?
+
+# gList class - a map of grocery items
+# methods:
+#   Initialize - hash with item names as key, item data in an array as hash value
+#   Add - add a new item to the list, its status, price, category
+#   Remove - remove an item from the list entirely
+
+# Release 2: Write Driver Code
+# Translate your pseudocode into driver code. Write driver code at the bottom (to call the method on the object).
+
+test = Glist.new
+test.add("Banana", 1.00, "Fruit", false)
+test.add("Strawberry", 2.00, "Fruit", false)
+test.viewList()
+test.remove("Strawberry")
+test.viewList()
+puts test.remove("Steak") == "Steak not found."
+test.add("Strawberry", 2.00, "Fruit", false)
+test.pickupItem("Strawberry") 
+test.viewList()
+test.dropItem("Strawberry")
+test.viewList()
+
+# Release 3: Initial Solution
+# Create your initial solution.
+
+class Glist 
+    def initialize()
+        @items = Hash.new
+    end
+    
+    def add(item, price, category, status)
+        item_info = [price, category, status]
+        @items[item] = item_info
+    end
+    
+    def remove(item)
+        if (@items.include?(item))
+            @items.delete(item)
+        else
+            puts item.to_s + " not found."
+            return item.to_s + " not found."
+        end
+    end
+    
+    def viewList()
+        puts "Grocery list:"
+        count = 1
+        @items.each_pair do |item, item_info|
+            print count.to_s + ". " + item + ": ("
+            item_info.each do |x|
+                print x.to_s
+                if (x != item_info.last) 
+                    print ", "
+                end 
+            end
+            puts ")"
+            count = count + 1
+        end
+    end
+end
+
+# Release 4: Refactor your solution
+# With your partner, review the code. Is it DRY? Are the names good?
+
+class Glist 
+    def initialize()
+        @items = Hash.new
+    end
+    
+    def add(item, price, category, status)
+        item_info = [price, category, status]
+        @items[item] = item_info
+    end
+    
+    def remove(item)
+        if (@items.include?(item))
+            @items.delete(item)
+        else
+            puts item.to_s + " not found."
+            return item.to_s + " not found."
+        end
+    end
+    
+    def viewList()
+        puts "Grocery list:"
+        count = 1
+        @items.each_pair do |item, item_info|
+            print count.to_s + ". " + item + ": ("
+            item_info.each do |x|
+                print x.to_s
+                if (x != item_info.last) 
+                    print ", "
+                end 
+            end
+            puts ")"
+            count = count + 1
+        end
+    end
+    
+    def pickupItem(item)
+        if(@items.include?(item))
+            @items[item][2] = true
+        end
+    end
+    
+    def dropItem(item)
+        if(@items.include?(item))
+            @items[item][2] = false
+        end
+    end
+end
 
 
-
-
-
-
-
-# DRIVER CODE GOES HERE. 
- 
- 
- 
+# Reflection 
