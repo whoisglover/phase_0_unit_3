56457810bb9ac664981f0301015fda3b633be2ad
whoisglover <dannyglover@outlook.com>
2014-05-26 00:55:43 -0700

    super_fizz

--- week_8_and_9/3_Ruby/fizz_buzz/my_solution.rb
+++ week_8_and_9/3_Ruby/fizz_buzz/my_solution.rb
@@ -5,37 +5,46 @@
 
 # 2. Pseudocode
 
-# 1. create method super_fizzbuzz with a single argument (which should be an array)
-# 2. map across the array checking if each element is divisible by 15, 5, and 3
-# 3. if number is divisible by 15, replace it with "FizzBuzz"
-# 4. if number is divisible by 5, replace it with "Buzz"
-# 5. if number is divisible by 3, replace it with "Fizz"
-# 6. put all that in an if statement
+#define new method super_bizzbuzz - it will accept an array of ints
+# loop thru array and check each number for divisibilty wiht 15, 5, and 3 in that order
+# if it is divisible by one of these three then replace with the apporpirate word
+# if it is not leave it alone
 
 
 
 # 3. Initial Solution
 
-def super_fizzbuzz(array)
-  array.map do |x|
-    if x % 15 == 0
+def super_fizzbuzz(input_array)
+  input_array.map do |input_num|
+    if input_num % 15 == 0
       "FizzBuzz"
-    elsif x % 5 == 0
+    elsif input_num % 5 == 0
       "Buzz"
-    elsif x % 3 == 0
+    elsif input_num % 3 == 0
       "Fizz"
     else
-      x
+      input_num
     end
   end
 end
 
 
-
 # 4. Refactored Solution
 
-# I'm honestly not sure how to make this any simpler. 
-
+# Refactored from map to map!  This modifys the array in place and thus takes up more temp memory (not sure if its enough to care)
+def super_fizzbuzz(input_array)
+  input_array.map! do |input_num|
+    if input_num % 15 == 0
+      "FizzBuzz"
+    elsif input_num % 5 == 0
+      "Buzz"
+    elsif input_num % 3 == 0
+      "Fizz"
+    else
+      input_num
+    end
+  end
+end
 
 
 
@@ -44,11 +53,15 @@ end
 array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 array2 = [5, 10, 15, 20, 25, 30, 35, 40]
 array3 = [45, 10, 17, 3, 9, 50, 11, 18, 12, 0]
-
+p array1
 p super_fizzbuzz(array1) == [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"]
+p array1
+p array2
 p super_fizzbuzz(array2) == ["Buzz", "Buzz", "FizzBuzz", "Buzz", "Buzz", "FizzBuzz", "Buzz", "Buzz"]
+p array2
+p array3
 p super_fizzbuzz(array3) == ["FizzBuzz", "Buzz", 17, "Fizz", "Fizz", "Buzz", 11, "Fizz", "Fizz", "FizzBuzz"]
-
+p array3 
 
 
 
